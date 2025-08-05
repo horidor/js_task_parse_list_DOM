4a6f076ff614eb2f8e58ef0c3ed4df861f2800b1
Maxim <makson202007@gmail.com>
2025-08-05 11:09:22 +0300

    added comment clarifying the code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,6 +19,8 @@ function sortList(list) {
 }
 
 function getEmployees(list) {
+  // There is no dataset.name value in index.html, and I can't change index.html.
+  // Therefore, i can only access it with item.innerText.
   return Array.from(list.children).map((item) => ({
     ...item.dataset,
     name: item.innerText,
